8eefdcb0f18d5209bc2acd4e2e40664e04593f45
john <john.kellner83@googlemail.com>
2016-11-26 20:01:05 +0100

    Update Service Frequently

---
+++ app/src/main/java/rottmar/project/com/appwidgetclock/activity/ClockWidget.java
@@ -0,0 +1,83 @@
+package rottmar.project.com.appwidgetclock.activity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.Calendar;
+
+import rottmar.project.com.appwidgetclock.service.ClockService;
+import rottmar.project.com.appwidgetclock.R;
+import rottmar.project.com.appwidgetclock.util.DateUtil;
+
+import static android.widget.Toast.makeText;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ClockWidget extends AppWidgetProvider {
+    private static PendingIntent service = null;
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock_widget);
+        views.setTextViewText(R.id.appwidget_text, new DateUtil().getTime());
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+        Log.d(ClockWidget.class.getName(), "frequently update");
+        alarm(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction().equals("android.intent.action.SCREEN_ON")){
+            Log.d(ClockWidget.class.getName(), "screenOn");
+        }
+        Log.d(ClockWidget.class.getName(), intent.getAction());
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public static void alarm(Context context){
+        final AlarmManager m = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        final Calendar TIME = Calendar.getInstance();
+        TIME.set(Calendar.MINUTE, 0);
+        TIME.set(Calendar.SECOND, 0);
+        TIME.set(Calendar.MILLISECOND, 0);
+
+        final Intent i = new Intent(context, ClockService.class);
+
+        if (service == null)
+        {
+            service = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);
+        }
+
+        m.setRepeating(AlarmManager.RTC, TIME.getTime().getTime(), 1000, service);
+        Log.d(ClockWidget.class.getName(), "alarm");
+    }
+}
+
---
+++ app/src/main/java/rottmar/project/com/appwidgetclock/service/ClockService.java
@@ -0,0 +1,51 @@
+package rottmar.project.com.appwidgetclock.service;
+
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import rottmar.project.com.appwidgetclock.R;
+import rottmar.project.com.appwidgetclock.activity.ClockWidget;
+import rottmar.project.com.appwidgetclock.util.DateUtil;
+
+/**
+ * Created by john on 26.11.2016.
+ */
+
+public class ClockService extends Service {
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        buildUpdate();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void buildUpdate()
+    {
+        RemoteViews view = new RemoteViews(getPackageName(), R.layout.clock_widget);
+        view.setTextViewText(R.id.appwidget_text, new DateUtil().getTime());
+
+        // Push update for this widget to the Clock screen
+        ComponentName thisWidget = new ComponentName(this, ClockWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+        manager.updateAppWidget(thisWidget, view);
+        Log.d(ClockService.class.getName(),"ClockService Call");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
